05639a9682e936a982eee338d8f916651d47920c
Henrique Costa <henri-kun@outlook.jp>
2018-02-01 12:14:03 -0200

    update
    update

--- main_script_projeto.R
+++ main_script_projeto.R
@@ -16,21 +16,19 @@ is.list(dados.municipios.mt)
 # install.packages(sidrar)
 require(sidrar)
 
-# Baixar a tabela lavouras temporarias cultivadas em Acorizal
-lav.temporaria_Acorizal = get_sidra(api='/t/1612/p/last/n6/5100102/c81/allxt')
-
-# tab5457_Acorizal = get_sidra(api='/t/5457/p/last/n6/5100102/c782/allxt')
+# Baixar a tabela lavouras temporárias e permanentes cultivadas em Acorizal
+tab5457_Acorizal = get_sidra(api='/t/5457/p/all/n6/5100102/c782/allxt')
 
 # linha de codigo para remover colunas indesejadas
-lav.temporaria_Acorizal <- lav.temporaria_Acorizal[,-c(1,3,4,7,9)]
+tab5457_Acorizal <- tab5457_Acorizal[,-c(1,3,4,7,9)]
 
 # renomear uma coluna (variavel)
-names(lav.temporaria_Acorizal)[2] <- "Código"
-names(lav.temporaria_Acorizal)
+names(tab5457_Acorizal)[2] <- "Código"
+names(tab5457_Acorizal)
 # Colocar o dataframe "Acorizal" dentro da lista "municipios_mt" 
 # comando 'complete.case' para limpar o dataframe removendo
 # as linhas que continham valores NA's 
-dados.municipios.mt[[1]] <- lav.temporaria_Acorizal[complete.cases(lav.temporaria_Acorizal), ]
+dados.municipios.mt[[1]] <- tab5457_Acorizal[complete.cases(tab5457_Acorizal), ]
 
 
 
