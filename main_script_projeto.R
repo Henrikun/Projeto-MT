5c07b327bd92bd79e7973e4b86f025ac75839a2b
Henrique Costa <henri-kun@outlook.jp>
2018-02-06 18:02:09 -0200

    update
    update do script

--- main_script_projeto.R
+++ main_script_projeto.R
@@ -2,57 +2,66 @@
 #   https://github.com/Henrikun/Projeto-MT
 
 
-#install.packages(repmis)
+# install.packages('repmis')
+# install.packages('sidrar')
 require(repmis)
-# linha de codigo para baixar o dataframe dos municipios de Mato Grosso
-source_data("https://github.com/Henrikun/Projeto-MT/blob/master/municipios_mt.Rdata?raw=true")
-
-View(municipios_mt)
-
-#  Transformar o dataframe em lista com dataframe
-dados.municipios.mt <- as.list(municipios_mt$Município)
-is.list(dados.municipios.mt)
-
-# install.packages(sidrar)
+library(dplyr)
 require(sidrar)
 
-# Baixar a tabela lavouras temporárias e permanentes cultivadas em Acorizal
-tab5457_Acorizal = get_sidra(api='/t/5457/p/all/n6/5100102/c782/allxt')
+# linha de codigo para baixar o dataframe dos municipios de Mato Grosso
+# source_data("https://github.com/Henrikun/Projeto-MT/blob/master/municipios_mt.Rdata?raw=true")
 
-# linha de codigo para remover colunas indesejadas
-tab5457_Acorizal <- tab5457_Acorizal[,-c(1,3,4,7,9)]
+load('municipios_mt.Rdata')
 
-# renomear uma coluna (variavel)
-names(tab5457_Acorizal)[2] <- "Código"
-names(tab5457_Acorizal)
+municipios_mt
+
+links_muni <- paste0('/t/5457/p/all/n6/',municipios_mt$Código,'/c782/allxt')
+
+# TODO
+# retirar [1:5] para baixar os dados de todos os municípios
+munid1 <- lapply(links_muni[1:5], function(x) get_sidra(api=x))
+
+length(munid1)
+
+head(munid1[[1]])
+head(munid1[[2]])
+
+munid1[[1]][1:5,1:4]
+
+lapply(munid1,names)
+lapply(munid1,head)
+lapply(munid1,dim)
+
+for(i in seq_along(munid1)){
+    print(head(munid1[[i]]))
+    #     print(i)
+}
+
+munid2 <- list()
+
+for(i in seq_along(munid1)){
+    munid2[[i]] <- munid1[[i]][,-c(1,3,4,7,9)]
+    names(munid2[[i]])[2] <- "Código"
+    munid2[[i]] <- munid2[[i]][complete.cases(munid2[[i]]), ]
+}
+
+lapply(munid2,head)
+lapply(munid2,dim)
 
 # Tabela com a população do censo de 2010 para os municípios brasileiros
 tab200 = get_sidra(api = "/t/200/p/last/n6/all")
 
-
-library(dplyr)
 #  Filtrando apenas os municípios de Mato Grosso
 tt2 <- tab200 %>% 
   filter(tab200$`Município (Código)` %in% municipios_mt$Código)
 
-# Adicionando as informações filtradas na lista de municípios de MT
+
+# existe um perigo, pois, os códigos dos municípios podem estar em ordem desigual
+# para superar essa dificuldade é possível utilizar a função merge do pacote R base
+# ou a função left_join do pacote dplyr
+
+dim(municipios_mt);dim(tt2)
+
 municipios_mt[,4:5] <- c(tt2[,2], tt2[,15])
 names(municipios_mt)[4:5] <- c(names(tt2)[2], tt2[1,6])
-
-# Colocar o dataframe "Acorizal" dentro da lista "municipios_mt" 
-# comando 'complete.case' para limpar o dataframe removendo
-# as linhas que continham valores NA's 
-dados.municipios.mt[[1]] <- tab5457_Acorizal[complete.cases(tab5457_Acorizal), ]
-
-
-
-
-
-
-
-
-
-
-
-
 
